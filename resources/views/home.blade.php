754d838bd7e6b009fe703525964bbe30dac7cb62
Kevin <kvnkaiser@gmail.com>
2018-07-24 22:40:45 +0200

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,19 @@
 
 @section('sidebar')
 
-    {{ $front_page_work->title_short }}<br>
-    <small class="text-muted">
-        {{ $front_page_work->materials_used }}, {{ $front_page_work->year }}
-    </small>
+    @isset($front_page_work)
+        {{ $front_page_work->title_short }}<br>
+            <small class="text-muted">
+                {{ $front_page_work->materials_used }}, {{ $front_page_work->year }}
+            </small>
+    @endisset
 
 @endsection
 
 @section('content')
 
-    <img src="{{ asset('storage/'.$front_page_work->cover_image) }}" class="img-fluid">
+    @isset($front_page_work)
+        <img src="{{ asset('storage/'.$front_page_work->cover_image) }}" class="img-fluid">
+    @endisset
 
 @endsection
