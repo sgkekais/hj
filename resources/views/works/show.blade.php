037207f2ad686613969bfaa52c3265dbed8540f5
sgkekais <kvnkaiser@gmail.com>
2019-08-26 00:19:27 +0200

    bugfix for images in safari

--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -30,6 +30,7 @@
                         <div class="embed-responsive embed-responsive-16by9">
                             <iframe class="embed-responsive-item" src="{{ $video }}" allowFullScreen mozallowfullscreen webkitAllowFullScreen></iframe>
                         </div>
+                        <div class="clearfix"></div>
                     </div>
                 </div>
 
